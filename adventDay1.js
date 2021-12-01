5ba11447ae6c2a606ecde7da76afadcf2a60192e
thewebgrem <thewebgrem@gmail.com>
2021-12-02 07:57:08 +1000

    Remove duplicate - prob from being tired

--- adventDay1.js
+++ adventDay1.js
@@ -9,18 +9,6 @@ const calculateTimesDepthIncreased = function(mesurements) {
   return timesIncreased;
 }
 
-
-const depthMesurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-const calculateTimesDepthIncreased = function(mesurements) {
-  let timesIncreased = 0;
-  mesurements.filter(function(item, index, array) {
-    if (item > array[index - 1]) {
-      timesIncreased++;
-    };
-  });
-  return timesIncreased;
-}
-
 // The first example with the result already confirmed
 console.log(calculateTimesDepthIncreased(depthMesurements));
 
